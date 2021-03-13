dfdcbfc9cc33f828985b8673df2aeb622e322649
WindS <windstorm_v2@hotmail.com>
2021-03-13 22:58:34 +0800

    V0.01

---
+++ src/main/java/per/lnonvo/hellogithub/HelloGitHub.java
@@ -0,0 +1,17 @@
+package per.lnonvo.hellogithub;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * @ClassName HelloGitHub
+ * @Description TODO
+ * @Author lijunfeng
+ * @Date 2021/3/13 22:52
+ * @Version 1.0
+ **/
+@Component
+public class HelloGitHub {
+    public void hello(){
+        System.out.println("Hello GitHub");
+    }
+}
---
+++ src/test/java/per/lnonvo/hellogithub/HellogithubApplicationTests.java
@@ -0,0 +1,13 @@
+package per.lnonvo.hellogithub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HellogithubApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
